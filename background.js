d900fed502d2221a3b6f50b0adf62341c5f80534
Ambushfall <lukathetormentor@gmail.com>
2022-12-29 12:45:28 +0100

    Modified

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
-        const scriptz = "https://example.com/path/to/external.js"
+        const scriptz = "https://cdn.jsdelivr.net/gh/Ambushfall/ext-external-script/external.js"
       // Execute the script in the page
       chrome.tabs.executeScript(tabId, {
         code: `var script = document.createElement("script"); script.src = ${scriptz}; document.body.appendChild(script);`
